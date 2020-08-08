15dfdaa038c72346a3e74686b5cc4874b7d01d50
mainul786 <53607383+mainul786@users.noreply.github.com>
2020-08-08 10:27:47 +0530

    true false uses system

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// falsey
+// 0;
+// NaN;
+// false;
+// "";
+// truly
+// {};
+// [];
+// ();
+// " ";
+// '0';
+
+const name='rasel';
+if(name){
+    console.log('condition is true');
+}else{
+    console.log('condition is false');
+}
